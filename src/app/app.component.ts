5b586de77c2c51423a244b97b5618865ed108624
mvdg-atr <110898656+mvdg-atr@users.noreply.github.com>
2022-10-12 21:44:16 +0200

    datasource with filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import DataSource from 'devextreme/data/data_source';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dataSource: DataSource;
   title = 'dx-app';
+
+  constructor() {
+    this.dataSource = new DataSource(
+      {
+        store: [
+          { name: "item1", value: 33 },
+          { name: "item2", value: 28 },
+          { name: "item3", value: 15 },
+          { name: "item4", value: 61 },
+          { name: "item5", value: 44 },
+          { name: "item6", value: 8 }
+        ],
+        filter: [
+          ["value", ">", "40"],
+          "or",
+          [
+            ["name", "=", "item3"],
+            "or",
+            ["name", "=", "item6"]
+          ]
+        ]
+      }
+    )
+  }
 }
