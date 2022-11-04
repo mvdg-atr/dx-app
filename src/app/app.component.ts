6b532027028edbbc37e8925039639c0b6940948b
mvdg-atr <110898656+mvdg-atr@users.noreply.github.com>
2022-11-04 18:20:31 +0100

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import DataSource from 'devextreme/data/data_source';
 
 
@@ -7,9 +7,25 @@ import DataSource from 'devextreme/data/data_source';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   dataSource: DataSource;
   title = 'dx-app';
+
+  ngOnInit(): void {
+    let person = {
+      firstName: 'Jim',
+      lastName: 'Cooper',
+      age: 18,
+      isAdult() {
+        return this.age >= 18
+      }
+    };
+
+    // console.log(Object.keys(person));
+    ;
+
+
+  }
 
   constructor() {
     this.dataSource = new DataSource(
@@ -22,16 +38,60 @@ export class AppComponent {
           { name: "item5", value: 44 },
           { name: "item6", value: 8 }
         ],
-        filter: [
-          ["value", ">", "40"],
-          "or",
-          [
-            ["name", "=", "item3"],
-            "or",
-            ["name", "=", "item6"]
-          ]
-        ]
+
+
+        // filter: [
+        // ["value", ">", "40"],
+        // "or",
+        // [
+        //   ["name", "=", "item3"],
+        //   "or",
+        //   ["name", "=", "item6"]
+        // ]
+        // ]
       }
     )
   }
+
+  // { item(filter: [
+  //   { or:
+  //     { value: "name", operator: "like", property: 6 },
+  //     { value: "name", operator: "like", property: 6 }
+  //   }
+  // ]
+
+  // groupOperator
+
+  onValueChange(filter: []) {
+    console.log(...filter);
+if(Array.isArray(filter)) {
+  console.log('isArray')
 }
+    // for (const item in filter) {
+    //   console.log(item);
+    // }
+
+    }
+
+
+    //
+    //     if (Array.isArray(filter[item])) {
+    //
+    //       Object.assign(obj, filter[item])
+    //       console.log('OBJ: ', obj);
+    //     } else {
+    //       console.log(item.toUpperCase());
+    //       return
+    //     }
+    //
+    //
+    //   }
+    //   //
+    //   // if (dxFilter[1] == "and" || "or") {
+    //   //   const obj = {}
+    //   // }
+    //   // console.log(dxFilter)
+    // }
+
+
+  }
